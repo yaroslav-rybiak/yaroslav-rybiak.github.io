58c7e1f2198957e4c3e04bb6c3b08ecfbcf5023a
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2016-02-18 11:55:58 +0200

    first commit

---
+++ js/hide-nav-on-click.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('.nav a').on('click', function(){
+        if($('.navbar-toggle').css('display') !='none'){
+            $(".navbar-toggle").trigger( "click" );
+        }
+    });
+});
